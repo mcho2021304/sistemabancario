70a335afbe9376f0154eec7fa2ed683111ecca0e
Mcho2021304 <mcho-2021304@kinal.edu.gt>
2026-02-24 21:41:28 -0600

    Se modifico la creacion de modulo Roles

--- src/Roles/rol.routes.js
+++ src/Roles/rol.routes.js
@@ -1,14 +1,12 @@
 'use strict';
 
-import { Router } from "express";
-import { getRoles } from "./role.controller.js";
+import { Router } from 'express';
+import { createRole, getRoles } from './rol.controller.js';
 
 const router = Router();
 
-// Rutas GET
-router.get(
-    '/', 
-    getRoles
-);
+// Rutas para Roles
+router.post('/', createRole);  // Crear un rol
+router.get('/', getRoles);     // Obtener todos los roles activos
 
 export default router;
